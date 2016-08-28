76b8775b05a4c30d666feb1b9c25b6336f7c8ddf
Willie Mok <wing001@gmail.com>
2016-08-28 14:15:32 -0700

    Conditional file added

---
+++ conditionals.py
@@ -0,0 +1,60 @@
+# Your_name = raw_input("What is your name?")
+
+# Your_partner_name = raw_input("What is your partner name?")
+
+# if (Your_name > Your_partner_name):
+#     print "My name is greater!"
+# elif(Your_partner_name > Your_name):
+#     print "Their name is greater!"
+# else:
+#     print "Our names are equal!"
+
+# date = int(raw_input("What is today's date?"))
+# if (date >= 15):
+#     print "Oh, we're halfway there!"
+# else:
+#     print "The month is still young."
+
+# weekday = raw_input("What is the date of the week?")
+# if (weekday == "Monday"):
+#     print "Yay class day!"
+# elif (weekday == "Tuesday"):
+#     print "Sigh, it's only Tuesday."
+# elif (weekday == "Wednesday"):
+#     print "Humpday!"
+# elif (weekday == "Thursday"):
+#     print "#tbt"
+# elif (weekday == "Friday"):
+#     print "TGIF!"
+# else:
+#     print "Yeah, It's the weekend!"
+
+# age = int(raw_input("What is your age?"))
+# if (age >= 21):
+#     print "I can go to a bar."
+# elif (age >= 18 and age < 21):
+#     print "I can vote but I cannot go to the bar."
+# else:
+#     print "I cannot vote or go to a bar"
+
+# Write a conditional to test if x is even AND y is even. If both numbers are 
+# even, print "Both numbers are even." If x is even AND y is odd, print "x is 
+# even and y is odd." If x is odd and y is even, print "x is odd and y is 
+# even." Otherwise, print "Both numbers are odd."
+# x = 8
+# y = 9
+
+# if (x % 2 == 0 and y % 2 ==0):
+#     print "Both numbers are even"
+# elif (x % 2 == 0 and y % 2 ==1):
+#     print "x is even and y is odd"
+# elif (x % 2 == 1 and y % 2 ==0):
+#     print "x is odd and y is even"
+# else:
+#     print "Both numbers are odd"
+
+color = raw_input("what is your favorite color?")
+if (color == 'blue' or color == 'yellow' or color == 'red'):
+    print "My favorite color is primary color"
+else:
+    print " My favorite color is not a secondary color"
